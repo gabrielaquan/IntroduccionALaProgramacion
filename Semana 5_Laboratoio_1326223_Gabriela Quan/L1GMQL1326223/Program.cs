a642736c08cd34b9335763b880f50cfd9a2d83ac
gabrielaquan <141659382+gabrielaquan@users.noreply.github.com>
2023-09-04 10:11:55 -0600

    Add files via upload

---
+++ Semana 5_Laboratoio_1326223_Gabriela Quan/L1GMQL1326223/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo soy GABRIELA");
+        Console.ReadKey();
+        }
+}
+static void Main(string[] args)
+{
+    Console.WriteLine("Hola Mundo");
+    Console Writeline("Soy GABRIELA");
+
+Console.Wrte("Hola Mundo");
+Console.Write("Soy GABRIELA");
+Console.ReadKey();
+}
+}
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Ingrese su nombre:");
+    string Nombre =Console.ReadLine():
+
+    Console.WriteLine("Hola Mundo");
+
+/*COMERNTARIOS*/
+
+Console.Write("Hola Mundo")
+    Console.WriteLine("soy GABRIELA")
+    Console.ReadKey();
+}
+}
