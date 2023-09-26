f665c2398ef7805768f737109fd0a4eec839bf5a
gabrielaquan <141659382+gabrielaquan@users.noreply.github.com>
2023-09-25 20:56:44 -0600

    Add files via upload

---
+++ Semana8_Laboratorio_1326223_Gabriela Quan/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Design;
+
+namespace L7_GMQL_1326223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero positivo");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int A = 0, B = 1, C = 0, i = 2;
+            string resultado = "";
+
+            if (N == 0)
+            {
+                resultado = A.ToString();
+                if (N > 1)
+                {
+                    resultado += B.ToString();
+                    while (i < N)
+                    {
+                        C = A + B;
+                        resultado += C.ToString();
+                        A = B;
+                        B = C;
+                        i++;
+                    }
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+
+                else
+                {
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Resultado: " + resultado);
+            }
+        }
+    }
+}
+
+
