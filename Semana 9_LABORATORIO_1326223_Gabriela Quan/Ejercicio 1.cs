2666b97dc74c7811c619c3a3fd64c614cc416ee8
gabrielaquan <141659382+gabrielaquan@users.noreply.github.com>
2023-10-02 17:37:07 -0600

    Add files via upload

---
+++ Semana 9_LABORATORIO_1326223_Gabriela Quan/Ejercicio 1.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L9_GMQL_1326223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero mayor a 0 <N>");
+            int numero = Convert.ToInt32(Console.ReadLine()); 
+            int contador = 1;
+            int sumatoria = 0;
+
+            do
+            {
+                sumatoria = sumatoria + contador;
+                contador++;
+            } while (contador <= numero);
+
+            Console.WriteLine("La suma desde 1 hasta el numero ingresado es: " + sumatoria);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Semana 9_LABORATORIO_1326223_Gabriela Quan/Ejercicio 2.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero para mostrar su tabla de multiplicar: ");
+            if (int.TryParse(Console.ReadLine(), out int numero))
+            {
+                Console.WriteLine($"Tabla de multiplicar de {numero}:");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    int total = numero * i;
+                    Console.WriteLine($"{numero} x {i} = {total}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR. Ingrese un numero entre el 1 al 10.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
